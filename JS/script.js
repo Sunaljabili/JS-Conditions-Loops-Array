59c0f556f4740a908cb63eaf064856def90b2901
Sunaljabili <you@example.com>
2021-10-28 01:06:48 +0400

    Js Loops,Conditions,Array

---
+++ JS/script.js
@@ -0,0 +1,134 @@
+// console.log("Salam");
+
+// let age =15;
+
+// if(age>16){
+//     console.log("Senin yashin 16dan coxdur");
+// }else{
+//     console.log("Senin yashin 16dan azdir");
+// }
+
+// let number =0;
+
+// if(number>0){
+//     console.log("Bu eded musbetdir");
+// }else if(number<0){
+//     console.log("Bu eded menfidir");
+// }else{
+//    console.log("Bu eded sifirdir")
+// }
+
+// let color ="green";
+
+// if(color =="goy"){
+//     console.log("Bu reng goydur");
+// }
+// else if(color =="red"){
+//     console.log("Bu reng  qirmizidir");
+// }else{
+// }
+
+// let number = 19;
+// switch (number) {
+//   case 19:
+//       alert("Bu reqem 19dir")
+//     break;
+//   case 17:
+//     console.log(17);
+//     break;
+//   case 18:
+//     console.log(18);
+//     break;
+//   default:
+//       console.log("Other Num");
+//     break;
+// }
+
+// let day = 4;
+// let dayName;
+
+// switch (day) {
+//   case 1:
+//     dayName = "Monday";
+//     break;
+//   case 2:
+//     dayName = "Tuesday";
+//     break;
+//   case 3:
+//     dayName = "Wednesday";
+//     break;
+//   case 4:
+//     dayName = "Thuesday";
+//     break;
+//   case 5:
+//     dayName = "Friday";
+//     break;
+//   case 6:
+//     dayName = "Satuday";
+//     break;
+//   case 7:
+//     dayName = "Sunday";
+//     break;
+//   default:
+//       dayName="Invalid day"
+//     break;
+// }
+
+// console.log(dayName);
+
+
+//Dövrlər -Loops   Emil 2-11  Nazile 2-11  Veli
+
+  //Dovurun Bashlangic noqtesi
+
+// while (i<=10) 
+// {
+//     i++;
+//     console.log(i); 
+// }
+
+
+// do {
+//     i++;
+//     console.log(i);
+// } while (i<1);
+
+
+// let i=0;
+// while(i<1){
+//     i++;
+//     console.log(i)
+// }
+
+// for(let i=0; i<10;i++){
+//     console.log(i);
+// }
+
+
+// let studentName1="Emil";
+// let studentName2="Nazile";
+// let studentName3="Veli";
+
+let studentArr =["Emil","Nazile","Veli","Sunal",undefined,null,15];
+// console.log(studentArr[5]);
+// console.log(studentArr.length);
+for (let i = 0; i <studentArr.length; i++) {
+  console.log(studentArr[i]);
+}
+
+// console.log(typeof studentArr);
+
+// let i =1;
+// console.log(typeof i);
+
+// let firstname ="Nazile";
+// console.log(typeof firstname);
+
+// let surname ="Jabili";
+// console.log(typeof surname); 
+
+// let isMarried =true;
+// console.log(typeof isMarried);
+
+// let a =10;
+// console.log(typeof a);
